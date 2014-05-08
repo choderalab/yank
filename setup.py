12d5b0cd9f57ef24040962ba7373f95806027f11
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-05-08 13:39:56 -0400

    Added version.py generation

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import os
 import sys
 from os.path import relpath, join
 from setuptools import setup, find_packages
+import subprocess
 DOCLINES = __doc__.split("\n")
 
 ########################
@@ -73,6 +74,77 @@ def check_dependencies():
         print()
         print(os.linesep.join([line for e in msg for line in textwrap.wrap(e)]), file=sys.stderr)
         #print('\n'.join(list(textwrap.wrap(e) for e in msg)))
+
+
+
+
+################################################################################
+# Writing version control information to the module
+################################################################################
+
+def git_version():
+    # Return the git revision as a string
+    # copied from numpy setup.py
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+
+def write_version_py(filename='yank/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM yank setup.
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+
+if not release:
+    version = full_version
+"""
+    # Adding the git rev number needs to be done inside write_version_py(),
+    # otherwise the import of numpy.version messes up the build under Python 3.
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev-' + GIT_REVISION[:7]
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version': FULLVERSION,
+                       'git_revision': GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+
+write_version_py()
+
+
 
 setup(
     name='yank',
