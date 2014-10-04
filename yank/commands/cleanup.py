aa0ab1afa52955065428d5602752709771615a8f
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-10-04 16:47:54 -0400

    Drafted implementation of cleanup.

--- yank/commands/cleanup.py
+++ yank/commands/cleanup.py
@@ -13,13 +13,18 @@ Clean up files produced by a YANK calculation.
 # MODULE IMPORTS
 #=============================================================================================
 
+import os, os.path
+import glob
 import sys
-import docopt
 
 #=============================================================================================
 # COMMAND DISPATCH
 #=============================================================================================
 
 def dispatch(args):
-    raise Exception("Not implemented.")
+    # Remove NetCDF files in the destination directory.
+    for filename in glob.glob(os.path.join(args['--store'], '*.nc')):
+        print "Removing file %s" % filename
+        os.remove(filename)
 
+    return True
