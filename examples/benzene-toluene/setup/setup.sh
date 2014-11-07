847daca350eda8bbb503eb21db7a3bdb1b429837
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-11-07 12:06:02 -0500

    Updated benzene-toluene example for updated command-line YANK.

--- examples/benzene-toluene/setup/setup.sh
+++ examples/benzene-toluene/setup/setup.sh
@@ -11,9 +11,3 @@ parmchk -i toluene.gaff.mol2 -o toluene.frcmod -f mol2
 # Create benzene-toluene system.
 rm -f leap.log receptor.{crd,prmtop,pdb} ligand.{crd,prmtop,pdb}
 tleap -f setup.leap.in
-
-# Create PDB files.
-cat receptor.crd | ambpdb -p receptor.prmtop > receptor.pdb
-cat ligand.crd | ambpdb -p ligand.prmtop > ligand.pdb
-cat complex.crd | ambpdb -p complex.prmtop > complex.pdb
-
