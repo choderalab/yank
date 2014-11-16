7b0894fa3058d8dd1b6e8ba5cdf9af1991194acd
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-11-15 21:06:42 -0500

    Fixed tip3p-in-tip3p example.

--- examples/tip3p-in-tip3p/setup/setup.sh
+++ examples/tip3p-in-tip3p/setup/setup.sh
@@ -5,3 +5,4 @@ echo "Creating AMBER prmtop/inpcrd files..."
 rm -f leap.log receptor.{inpcrd,prmtop,pdb} ligand.{inpcrd,prmtop,pdb} complex.{inpcrd,prmtop,pdb}
 tleap -f setup.leap.in > setup.leap.out
 
+
