de4b3153aec694ff74d1e61d16c64156e68c7c02
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-11-16 22:29:57 -0500

    Updated a number of examples to new naming convention

--- examples/tip3p-in-tip3p/setup/setup.sh
+++ examples/tip3p-in-tip3p/setup/setup.sh
@@ -2,7 +2,7 @@
 
 # Create AMBER prmtop/inpcrd files.
 echo "Creating AMBER prmtop/inpcrd files..."
-rm -f leap.log receptor.{inpcrd,prmtop,pdb} ligand.{inpcrd,prmtop,pdb} complex.{inpcrd,prmtop,pdb}
+rm -f leap.log {vacuum,solvent,complex}.{inpcrd,prmtop,pdb}
 tleap -f setup.leap.in > setup.leap.out
 
 
