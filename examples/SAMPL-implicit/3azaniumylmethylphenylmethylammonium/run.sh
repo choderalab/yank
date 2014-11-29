668e113fb6c5b45fba1407f62a2d362d8613ae0f
pgrinaway <win98pro>
2014-11-28 22:30:56 -0500

    added implicit solvent SAMPL systems. the host is already parameterized. One molecule was not named properly by openeye. will look into manually later

---
+++ examples/SAMPL-implicit/3azaniumylmethylphenylmethylammonium/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -e output ]; then
+    echo "Making output directory..."
+    mkdir output
+fi
+
+# Clean up any leftover files
+echo "Cleaning up previous simulation..."
+yank cleanup --store=output
+
+# Set up calculation.
+echo "Setting up binding free energy calculation..."
+yank setup binding amber --setupdir=setup --ligname=MOL --store=output --iterations=1000 --restraints=harmonic --gbsa=OBC2 --temperature=300*kelvin --verbose
+
+# Run the simulation with verbose output:
+echo "Running simulation..."
+yank run --store=output --verbose
+
