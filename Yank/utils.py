b036d184ba8b689d4893408d7e400e72a6622ec6
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-12-09 17:19:23 -0500

    Slightly improved docstrings / comments

--- Yank/utils.py
+++ Yank/utils.py
@@ -4,15 +4,16 @@ import os
 def get_data_filename(relative_path):
     """Get the full path to one of the reference files shipped for testing
 
-    In the source distribution, these files are in ``gaff2xml/chemicals/*/``,
+    In the source distribution, these files are in ``examples/*/``,
     but on installation, they're moved to somewhere in the user's python
     site-packages directory.
 
     Parameters
     ----------
     name : str
-        Name of the file to load (with respect to the gaff2xml folder).
-
+        Name of the file to load, with respect to the yank egg folder which
+        is typically located at something like
+        ~/anaconda/lib/python2.7/site-packages/yank-*.egg/examples/
     """
 
     fn = resource_filename('yank', relative_path)
--- setup.py
+++ setup.py
@@ -162,8 +162,7 @@ setup(
     classifiers=CLASSIFIERS.splitlines(),
     package_dir={'yank': 'Yank'},
     packages=['yank', "yank.tests", "yank.commands"] + ['yank.%s' % package for package in find_packages('yank')],
-    package_data={'yank': find_package_data('examples', 'yank')},
-    #package_data={'openmmtools': find_package_data('openmmtools/data', 'openmmtools')},
+    package_data={'yank': find_package_data('examples', 'yank')},  # NOTE: examples installs to yank.egg/examples/, NOT yank.egg/yank/examples/.  You need to do utils.get_data_filename("../examples/*/setup/").
     zip_safe=False,
     install_requires=[
         'numpy',
