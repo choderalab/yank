03a7d9a07c1b0114b23ba703407356ca08ad6c87
pgrinaway <win98pro>
2015-01-15 16:46:44 -0500

    implementing function to run mixer

--- Yank/tests/test_mixing.py
+++ Yank/tests/test_mixing.py
@@ -19,17 +19,30 @@ def mix_replicas(n_swaps=100, n_states=16, u_kl=None):
         The number of times to call the mixing code (default 100)
     n_states : int (optional)
         The number of replica states to include (default 16)
-    u_kl : ndarray of floats (optional)
-        Energies for each state. If None, will be initialized to 0s
+    u_kl : n_states x n_states ndarray of float64 (optional)
+        Energies for each state. If None, will be initialized to zeros
 
     Returns
     -------
-    replica_swap_results : n_states x n_swaps ndarray
-       Contains the result of swapping replicas for each state in each swap attempt
+    Nij_proposed : n_states x n_states ndarray of np.int64
+        Contains the number of times a given swap was proposed
+    Nij_accepted : n_states x n_states ndarray of np.int64
+        Contains the number of times a given swap was accepted
     """
+
+    if u_kl is None:
+        u_kl = np.zeros([n_states, n_states], dtype=np.float64)
+    replica_states = range(n_states)
+    Nij_proposed =  np.zeros([n_states,n_states], dtype=np.int64)
+    Nij_accepted = np.zeros([n_states,n_states], dtype=np.int64)
+    for i in range(n_swaps):
+        mixing._mix_replicas_cython(n_states, replica_states, u_kl, Nij_proposed, Nij_accepted)
+    return Nij_proposed, Nij_accepted
+
+
 
 
 def test_even_mixing():
     pass
 
-def test_general_mixing()
+def test_general_mixing():
