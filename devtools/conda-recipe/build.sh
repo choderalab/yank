cb31ab8f8f7940c35ddb7add3a13998ec70313c0
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-02-01 20:56:02 -0500

    Updated conda recipe to install examples in anaconda/share/yank/examples

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -5,5 +5,5 @@ $PYTHON setup.py clean
 $PYTHON setup.py install
 
 # Eventually we want to push examples to some place like ~/anaconda/share/yank/examples/
-#mkdir $PREFIX/share/yank/
-#cp -r $RECIPE_DIR/../../examples/ $PREFIX/share/yank/
+mkdir $PREFIX/share/yank
+cp -r $RECIPE_DIR/../../examples $PREFIX/share/yank/
