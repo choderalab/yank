508f3e851a45a4e7a270a1c8dc67414103c9f1a1
andrrizzi <andrrizzi@gmail.com>
2015-06-15 16:20:45 -0400

    Added abl-imatinib-explicit example.

---
+++ examples/abl-imatinib-explicit/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set up simulation in serial mode.
+
+if [ ! -e output ]; then
+    echo "Making output directory..."
+    mkdir output
+fi
+
+# Clean up any leftover files
+echo "Cleaning up previous simulation..."
+yank cleanup --store=output
+
+# Set up calculation.
+echo "Setting up binding free energy calculation..."
+yank prepare binding amber --setupdir=setup --ligname=MOL --store=output --iterations=100 --restraints=harmonic --gbsa=OBC2 --temperature="300*kelvin" --minimize --verbose
+
+# Run the simulation with verbose output:
+echo "Running simulation..."
+yank run --store=output --verbose
