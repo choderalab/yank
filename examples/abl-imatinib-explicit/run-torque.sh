f78b64b8bc009755a33498403a226aa9727dfda3
John Chodera <choderaj@mskcc.org>
2015-06-16 17:27:51 -0400

    Updated Torque run script for Abl example.

---
+++ examples/abl-imatinib-explicit/run-torque.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#  Batch script for mpirun job on cbio cluster.
+#
+#
+# walltime : maximum wall clock time (hh:mm:ss)
+#PBS -l walltime=8:00:00
+#
+# join stdout and stderr
+#PBS -j oe
+#
+# spool output immediately
+#PBS -k oe
+#
+# specify queue
+#PBS -q gpu
+#
+# nodes: number of nodes
+#   ppn: how many cores per node to use
+#PBS -l nodes=1:ppn=4:gpus=4:shared
+#
+# export all my environment variables to the job
+##PBS -V
+#
+# job name (default = name of script file)
+#PBS -N abl-imatinib-explicit
+#
+# specify email
+#PBS -M jchodera@gmail.com
+#
+# mail settings
+#PBS -m n
+#
+
+cd $PBS_O_WORKDIR
+
+# Set up and run simulation in serial mode.
+
+if [ ! -e output ]; then
+    echo "Making output directory..."
+    mkdir output
+fi
+
+# Clean up any leftover files
+echo "Cleaning up previous simulation..."
+yank cleanup --store=output
+
+# Set up calculation.
+echo "Setting up binding free energy calculation..."
+yank prepare binding amber --setupdir=setup --ligname=MOL --store=output --iterations=100 --restraints=harmonic --temperature="300*kelvin" --pressure="1*atmosphere" --minimize --verbose
+
+# Run the simulation with verbose output:
+echo "Running simulation via MPI..."
+#yank run --store=output --verbose
+#mpirun -rmk pbs yank run --store=output --verbose --mpi
+build_mpirun_configfile "yank run --store=output --verbose --mpi"
+mpirun -configfile configfile
+date
+
+
