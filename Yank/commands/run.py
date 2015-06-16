383e9dd138f82b5bf844648ca035251b9c3684f3
John Chodera <choderaj@mskcc.org>
2015-06-16 17:52:07 -0400

    Bugfix for MPI

--- Yank/commands/run.py
+++ Yank/commands/run.py
@@ -62,7 +62,7 @@ def dispatch(args):
         hostname = os.uname()[1]
         MPI.COMM_WORLD.barrier()
         logger.info("Initialized MPI on %d processes." % (MPI.COMM_WORLD.size))
-        mpicomm = MPI
+        mpicomm = MPI.COMM_WORLD
 
     # Run simulation.
     yank.run(mpicomm=mpicomm, options=options)
