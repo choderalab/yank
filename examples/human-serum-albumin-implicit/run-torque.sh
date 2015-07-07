ba072e1f43d365cb0b21613472070d0823cb2a87
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-07-06 23:24:24 -0400

    Added HSA examples.

---
+++ examples/human-serum-albumin-implicit/run-torque.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#  Batch script for mpirun job on cbio cluster.
+#
+#
+# walltime : maximum wall clock time (hh:mm:ss)
+#PBS -l walltime=24:00:00
+#
+# join stdout and stderr
+#PBS -j oe
+#
+# spool output immediately
+#PBS -k oe
+#
+# specify queue
+#PBS -q gpu
+#
+# nodes: number of 8-core nodes
+#   ppn: how many cores per node to use (1 through 8)
+#       (you are always charged for the entire node)
+#PBS -l nodes=1:ppn=4:gpus=4:shared
+#
+# export all my environment variables to the job
+##PBS -V
+#
+# job name (default = name of script file)
+#PBS -N p-xylene
+
+cd $PBS_O_WORKDIR
+
+# Set defaults
+export NITERATIONS=${NITERATIONS:=1000}
+
+if [ ! -e output ]; then
+    echo "Making output directory..."
+    mkdir output
+fi
+
+# Clean up any leftover files
+echo "Cleaning up previous simulation..."
+yank cleanup --store=output
+
+# Set up calculation.
+echo "Setting up binding free energy calculation..."
+yank prepare binding amber --setupdir=setup --ligand="resname MOL" --store=output --iterations=1 --restraints=harmonic --gbsa=OBC2 --temperature="300*kelvin" --verbose
+
+# Run the simulation with verbose output:
+echo "Running simulation via MPI..."
+build_mpirun_configfile  --mpitype=conda "yank run --store=output --verbose --mpi"
+mpirun -configfile configfile
+
+# Analyze the data
+echo "Analyzing data..."
+yank analyze --store=output
