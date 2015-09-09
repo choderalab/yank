89756c34b423156ff2b5f6c2ee5c3647219a5c24
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-09-09 15:46:22 +0200

    Update travis to docker infrastructure

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -11,8 +11,8 @@ PIP_ARGS="-U"
 
 export PATH=$HOME/miniconda/bin:$PATH
 
-sudo apt-get update
-sudo apt-get install -qq -y g++ gfortran csh g++-multilib gcc-multilib openbabel
+#sudo apt-get update
+#sudo apt-get install -qq -y g++ gfortran csh g++-multilib gcc-multilib openbabel
 
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
