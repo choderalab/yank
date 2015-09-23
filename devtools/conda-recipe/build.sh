3e1433da5227f7dceae335d64c7887685717b1c4
John Chodera <choderaj@mskcc.org>
2015-09-23 07:24:32 -0400

    Attempt to import conda test environment instead of re-installing local conda package for testing

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#export CC=${PREFIX}/bin/gcc
+#export CXX=${PREFIX}/bin/g++
+
+# conda provides default values of these on Mac OS X,
+# but we don't want them when building with gcc
+#export CFLAGS=""
+#export CXXFLAGS=""
+#export LDFLAGS=""
+
+# Build the python package
 $PYTHON setup.py install
 
 # Push examples to anaconda/share/yank/examples/
