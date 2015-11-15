07968d25f56b20ad4c0b9fa80a34766da7506bac
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-11-15 17:25:47 -0500

    Update travis infrastructure.

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -1,4 +1,7 @@
 
+pushd .
+cd $HOME
+
 MINICONDA=Miniconda-latest-Linux-x86_64.sh
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget http://repo.continuum.io/miniconda/$MINICONDA
@@ -10,11 +13,7 @@ bash $MINICONDA -b
 PIP_ARGS="-U"
 
 export PATH=$HOME/miniconda/bin:$PATH
-
-#sudo apt-get update
-#sudo apt-get install -qq -y g++ gfortran csh g++-multilib gcc-multilib openbabel
-
 conda update --yes conda
-conda config --add channels http://conda.binstar.org/omnia
-conda config --add channels https://conda.binstar.org/rdkit
-conda install --yes conda-build jinja2 binstar pip
+conda install --yes conda-build jinja2 anaconda-client pip
+
+popd
