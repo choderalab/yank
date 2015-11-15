a0a540ac259339387778c244c28b9ca71480b54d
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-11-15 17:28:29 -0500

    Another travis update

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -4,14 +4,13 @@ cd $HOME
 
 MINICONDA=Miniconda-latest-Linux-x86_64.sh
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget http://repo.continuum.io/miniconda/$MINICONDA
+wget -q http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
-bash $MINICONDA -b
-PIP_ARGS="-U"
-
+bash $MINICONDA -b -p $HOME/miniconda
+export PIP_ARGS="-U"
 export PATH=$HOME/miniconda/bin:$PATH
 conda update --yes conda
 conda install --yes conda-build jinja2 anaconda-client pip
