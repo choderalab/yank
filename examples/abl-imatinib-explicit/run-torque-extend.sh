1b799e9092b55fb9b0c60210672c42778d7a13b7
John Chodera <choderaj@mskcc.org>
2016-05-17 23:27:27 -0400

    changes

--- examples/abl-imatinib-explicit/run-torque-extend.sh
+++ examples/abl-imatinib-explicit/run-torque-extend.sh
@@ -16,7 +16,7 @@
 #
 # nodes: number of nodes
 #   ppn: how many cores per node to use
-#PBS -l nodes=2:ppn=4:gpus=4:shared
+#PBS -l nodes=1:ppn=4:gpus=4:shared
 #
 # export all my environment variables to the job
 ##PBS -V
