993735bbd03abf3a10fb3c17c386642b271bcc3e
Andrea Rizzi <andrrizzi@gmail.com>
2016-06-08 16:19:22 -0400

    Remove unused class YankOptions

--- Yank/tests/test_utils.py
+++ Yank/tests/test_utils.py
@@ -158,34 +158,6 @@ def test_temp_cd_context():
             assert os.getcwd() == os.path.realpath(tmp_dir)
         assert os.getcwd() != os.path.realpath(tmp_dir)
 
-def test_yank_options():
-    """Test option priorities and handling."""
-
-    cl_opt = {'option1': 1}
-    yaml_opt = {'option1': 2, 'option2': 'test'}
-    default_yank_opt = YankOptions()
-    yank_opt = YankOptions(cl_opt=cl_opt, yaml_opt=yaml_opt)
-
-    assert yank_opt['option2'] == 'test'
-    assert yank_opt['option1'] == 1  # command line > yaml
-    assert len(yank_opt) == len(default_yank_opt) + 2, "Excepted two additional options beyond default, found: %s" % str([x for x in yank_opt])
-
-    # runtime > command line
-    yank_opt['option1'] = 0
-    assert yank_opt['option1'] == 0
-
-    # restore old option when deleted at runtime
-    del yank_opt['option1']
-    assert yank_opt['option1'] == 1
-
-    # modify specific priority level
-    yank_opt.default = {'option3': -2}
-    assert len(yank_opt) == 3
-    assert yank_opt['option3'] == -2
-
-    # test iteration interface
-    assert yank_opt.items() == [('option1', 1), ('option2', 'test'), ('option3', -2)]
-    assert yank_opt.keys() == ['option1', 'option2', 'option3']
 
 def test_underscore_to_camelcase():
     """Test underscore_to_camelCase() conversion function."""
