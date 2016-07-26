ce9c8c09ac8242679df81f56114577c9c60bfb84
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-07-26 09:35:19 +0200

    Re-enable docs push to S3

--- devtools/travis-ci/after_success.sh
+++ devtools/travis-ci/after_success.sh
@@ -2,7 +2,7 @@
 # Must be invoked with $PACKAGENAME
 
 echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
-PUSH_DOCS_TO_S3=false
+PUSH_DOCS_TO_S3=true
 
 if [ "$TRAVIS_PULL_REQUEST" = true ]; then
     echo "This is a pull request. No deployment will be done."; exit 0
@@ -16,13 +16,13 @@ fi
 
 # Deploy to binstar
 conda install --yes anaconda-client jinja2
-binstar -t $BINSTAR_TOKEN upload --force -u omnia -p ${PACKAGENAME}-dev $HOME/miniconda/conda-bld/*/${PACKAGENAME}-dev-*.tar.bz2
+anaconda -t $BINSTAR_TOKEN upload --force -u omnia -p ${PACKAGENAME}-dev $HOME/miniconda/conda-bld/*/${PACKAGENAME}-dev-*.tar.bz2
 
 if [ $PUSH_DOCS_TO_S3 = true ]; then
    # Create the docs and push them to S3
    # -----------------------------------
     conda install --yes pip
-    conda config --add channels http://conda.binstar.org/omnia
+    conda config --add channels omnia
     conda install --yes `conda build devtools/conda-recipe --output`
     pip install numpydoc s3cmd msmb_theme
     conda install --yes `cat docs/requirements.txt | xargs`
