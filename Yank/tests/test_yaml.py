d8b78f33af4d9a205dc977948cdcf39b592c9855
Levi Naden <levi.naden@choderalab.org>
2016-09-16 16:25:50 -0400

    Trying to see why yaml fails

--- Yank/tests/test_yaml.py
+++ Yank/tests/test_yaml.py
@@ -1368,7 +1368,8 @@ def test_charged_ligand():
         explicit-system:
             receptor: !Combinatorial {}
             ligand: imatinib
-        """.format(imatinib_path, receptors.keys()))
+        """.format(imatinib_path, [key for key inreceptors.keys()]))
+        #""".format(imatinib_path, receptors.keys()))
         yaml_content = get_template_script(tmp_dir)
         yaml_content['molecules'].update(updates['molecules'])
         yaml_content['systems']['explicit-system'].update(updates['explicit-system'])
