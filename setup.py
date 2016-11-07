72e4945a0508261ac2d77f68188a0f7aa5e2cd03
Levi Naden <levi.naden@choderalab.org>
2016-11-07 17:19:45 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from Cython.Build import cythonize
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.12.0"
+VERSION = "0.12.1"
 ISRELEASED = False
 __version__ = VERSION
 ########################
