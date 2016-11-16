aa8546211c4a289242e0cbbf390a368ac8b4539b
Andrea Rizzi <andrrizzi@gmail.com>
2016-11-15 20:43:56 -0500

    Do not use periodic restraint if the system is not periodic

--- Yank/restraints.py
+++ Yank/restraints.py
@@ -366,7 +366,8 @@ class RadiallySymmetricRestraint(ReceptorLigandRestraint):
         """
         force = openmm.CustomBondForce(self._energy_function)
         force.addGlobalParameter('lambda_restraints', 1.0)
-        force.setUsesPeriodicBoundaryConditions(True)
+        is_periodic = self._system.usesPeriodicBoundaryConditions()
+        force.setUsesPeriodicBoundaryConditions(is_periodic)
         for parameter in self._bond_parameter_names:
             force.addPerBondParameter(parameter)
         try:
@@ -932,7 +933,8 @@ class Boresch(OrientationDependentRestraint):
         force = openmm.CustomCompoundBondForce(nparticles, energy_function)
         force.addGlobalParameter('lambda_restraints', 1.0)
         force.addBond(self._restraint_atoms, [])
-        force.setUsesPeriodicBoundaryConditions(True)
+        is_periodic = self._system.usesPeriodicBoundaryConditions()
+        force.setUsesPeriodicBoundaryConditions(is_periodic)
         return force
 
     def get_standard_state_correction(self):
