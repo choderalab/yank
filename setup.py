f8be5cb7da153a96019f98b4a497e66b0a66d9e4
Levi Naden <levi.naden@choderalab.org>
2016-11-18 20:28:27 -0500

    Merged from expanded cutoff changes, updated version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from Cython.Build import cythonize
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.12.3"
+VERSION = "0.14.0"
 ISRELEASED = False
 __version__ = VERSION
 ########################
