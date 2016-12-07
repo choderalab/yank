0f7a2b1fa3584cfbd34d1e746a5de7dcf08d6e3f
Levi Naden <levi.naden@choderalab.org>
2016-12-07 17:00:59 -0500

    trying new scheme for uploading docs

---
+++ devtools/travis-ci/build_docs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Print each line, exit on error
+set -ev
+
+# Install the built package
+conda create --yes -n docenv python=$CONDA_PY
+source activate docenv
+conda install -yq --use-local yank-dev
+
+# We don't use conda for these:
+pip install -I sphinx==1.3.5 sphinx_rtd_theme==0.1.9 msmb_theme==1.2.0
+
+# Install doc requirements
+conda install -yq --file docs/requirements.txt
+
+# Make docs
+cd docs && make html && cd -
+
+# Move the docs into a versioned subdirectory
+python devtools/travis-ci/set_doc_version.py
+
+# Prepare versions.json
+python devtools/travis-ci/update_versions_json.py
---
+++ devtools/travis-ci/set_doc_version.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+from yank import version
+
+if version.release:
+    docversion = version.version
+else:
+    docversion = 'latest'
+
+os.mkdir("docs/_deploy")
+shutil.copytree("docs/_build/html", "docs/_deploy/{docversion}"
+                .format(docversion=docversion))
