f678c2b6c7d4998f85af4fc30cb2c059bdfa9056
Levi Naden <levi.naden@choderalab.org>
2017-01-31 17:37:16 -0500

    Doc pushes should finally be fixed and behave as expected

--- devtools/travis-ci/set_doc_version.py
+++ devtools/travis-ci/set_doc_version.py
@@ -10,3 +10,15 @@ else:
 os.mkdir("docs/_deploy")
 shutil.copytree("docs/_build", "docs/_deploy/{docversion}"
                 .format(docversion=docversion))
+
+# Only update latest if we are on a release version
+if version.release:
+    # Update the "latest" index file
+    base_index_string = """<html><head><meta http-equiv="refresh" content="0;URL='/{WHEREISLATEST}'"/></head></html>"""
+    index_html = base_index_string.format(WHEREISLATEST=docversion)
+    try:
+        os.mkdir("docs/_deploy/latest")
+    except:
+        pass
+    with open("docs/_deploy/latest/index.html", 'w') as index_file:
+        index_file.write(index_html)
