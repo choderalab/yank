b58a0c458998eadabee5b2244596147d808b0301
Andrea Rizzi <andrea.rizzi@choderalab.org>
2017-11-30 16:00:46 -0500

    Bump to 0.19.3. Turn off release flag.

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ from Cython.Build import cythonize
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.19.2"  # Primary base version of the build
+VERSION = "0.19.3"  # Primary base version of the build
 DEVBUILD = "0"  # Dev build status, Either None or Integer as string
-ISRELEASED = True  # Are we releasing this as a full cut?
+ISRELEASED = False  # Are we releasing this as a full cut?
 __version__ = VERSION
 ########################
 CLASSIFIERS = """\
