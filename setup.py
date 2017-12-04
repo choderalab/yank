fb3b8dd906a390c562b060b182137504517580cf
Andrea Rizzi <andrea.rizzi@choderalab.org>
2017-12-04 12:45:36 -0500

    Release 0.19.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DOCLINES = __doc__.split("\n")
 ########################
 VERSION = "0.19.3"  # Primary base version of the build
 DEVBUILD = "0"  # Dev build status, Either None or Integer as string
-ISRELEASED = False  # Are we releasing this as a full cut?
+ISRELEASED = True  # Are we releasing this as a full cut?
 __version__ = VERSION
 ########################
 CLASSIFIERS = """\
