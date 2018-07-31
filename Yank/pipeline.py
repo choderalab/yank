08262005154d4462110d969a58e5dfa62b138d49
steven-albanese <albaness@lski1985.mskcc.org>
2018-07-31 16:19:11 -0400

    open draft to add modeller functionality

--- Yank/pipeline.py
+++ Yank/pipeline.py
@@ -33,6 +33,7 @@ from pdbfixer import PDBFixer
 from simtk import openmm, unit
 from simtk.openmm.app import PDBFile
 
+
 from . import utils, mpi
 
 logger = logging.getLogger(__name__)
@@ -896,6 +897,7 @@ def apply_pdbfixer(input_file_path, output_file_path, directives):
             pdbfixer_mutations = [generate_pdbfixer_mutation_code(*decompose_mutation(mutation))
                                   for mutation in mutations.split('/')]
             logger.info('pdbfixer: Will make mutations {} to chain_id {}.'.format(pdbfixer_mutations, chain_id))
+
             fixer.applyMutations(pdbfixer_mutations, chain_id)
         else:
             logger.info('pdbfixer: No mutations will be applied since "WT" specified.')
@@ -943,6 +945,111 @@ def apply_pdbfixer(input_file_path, output_file_path, directives):
     # Write the final structure
     PDBFile.writeFile(fixer.topology, fixer.positions, open(output_file_path, 'w'))
 
+def apply_modeller(input_file_path, output_file_path, directives):
+    """
+    Apply Salilab Modeller to make changes to the specified molecule.
+
+    Single mutants are supported in the form "T315I"
+    Double mutants are not supported currently.
+
+    The string "WT" makes no mutations.
+    This is useful for testing.
+
+    Original PDB file numbering scheme is used.
+
+    Currently, only PDB files are supported.
+    modeller is used to make the mutations. You must have a license file installed for this to work
+
+    Parameters
+    ----------
+    input_file_path : str
+        Full file path to the file to read, including extensions
+    output_file_path : str
+        Full file path to the file to save, including extensions
+    directives : dict
+        Dict containing directives for modeller.
+    """
+
+    try:
+        import modeller
+    except ImportError:
+        raise ImportError('Modeller and license must be installed to use this feature.')
+
+    directives = copy.deepcopy(directives)
+    env = modeller.environ()
+    atom_files_directory = os.path.dirname(input_file_path)
+    atom_file_name = os.path.basename(input_file_path)
+
+    env.io.atom_files_directory = [atom_files_directory]
+    alignment = modeller.alignment(env)
+    model = modeller.model(env, file=atom_file_name)
+    alignment.append_model(model, atom_files=atom_file_name, align_codes=atom_file_name)
+
+    def process_directive(option, dispatch, allowed_values, yields_value=False):
+        """Process a directive.
+
+        Parameters
+        ----------
+        option : str
+            The name of the option to be processed.
+            Will remove this option from `directives` once processed.
+        dispatch : function
+            The function to call.
+        allowed_values : list
+            If not None, the value of directives[option] will be checked against this list
+        yields_value : boolean, default False
+            Tells this function to expect a return from the dispatch function and give it back as needed
+        """
+        if option in directives:
+            value = directives[option]
+            # Validate options
+            if allowed_values is not None:
+                if value not in allowed_values:
+                    raise ValueError("'{}' must be one of {}".format(option, allowed_values))
+            # Dispatch
+            output = dispatch(value)
+            # Delete the key once we've processed it
+            del directives[option]
+            if yields_value:
+                return output
+            return
+
+    def apply_mutations_modeller(value):
+        # Extract chain id
+        chain_id = None
+        if 'chain_id' in value:
+            chain_id = value['chain_id']
+            if chain_id == 'none':
+                chain_id = None
+        # Extract mutations
+        mutations = value['mutations']
+        # Check that double mutants were not specified
+        if len(mutations) > 1:
+            raise ValueError("You have specified a double mutant, which is not supported by modeller")
+        # Convert mutations to PDBFixer format
+        if mutations != 'WT':
+            pdbfixer_mutations = [generate_pdbfixer_mutation_code(*decompose_mutation(mutation))
+                                  for mutation in mutations.split('/')]
+            logger.info('pdbfixer: Will make mutations {} to chain_id {}.'.format(pdbfixer_mutations, chain_id))
+            sel = modeller.selection(model.chains[chain_id].residues[pdbfixer_mutations[0].split('-')[1]])
+            sel.mutate(residue_type=pdbfixer_mutations[0].split('-')[2])
+            alignment.append_model(model, align_codes=pdbfixer_mutations[0])
+            model.clear_topology()
+            model.generate_topology(alignment[pdbfixer_mutations[0]])
+            model.transfer_xyz(alignment)
+            model.build(initialize_xyz=False, build_method='INTERNAL_COORDINATES')
+
+        else:
+            logger.info('pdbfixer: No mutations will be applied since "WT" specified.')
+
+    process_directive('apply_mutations', apply_mutations_modeller, None)
+
+    # Check that there were no extra options
+    if len(directives) > 0:
+        raise ValueError("The 'pdbfixer:' block contained some nodes that it didn't know how to process: {}".format(directives))
+
+    # Write the final model
+    model.write(file=output_file_path)
 
 def read_csv_lines(file_path, lines):
     """Return a list of CSV records.
