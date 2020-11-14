de648714663a4e7f38e52c51b8417b862fdcf535
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-11-14 12:48:07 +0100

    More __version__ patches

--- Yank/__init__.py
+++ Yank/__init__.py
@@ -8,9 +8,18 @@ YANK
 # Define global version.
 try:
     from . import version  # Needed for yank 3.X.
-    __version__ = version.version
-except ImportError:
-    __version__ = "0.0.0dev"
+except:
+    # Fill in information manually.
+    class _Version:
+        short_version = "dev"
+        version = "dev"
+        full_version = "dev"
+        git_revision = "dev"
+        release = False
+
+    version = _Version()
+
+__version__ = version.version
 
 # Self module imports
 from . import utils
